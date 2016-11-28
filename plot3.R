5bb5301a229c89ccfd814e172fafa5885e9dc9d1
PhoenixRavy <ravy.por@gmail.com>
2016-11-27 23:52:28 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -5,6 +5,8 @@ data$DateTime=as.POSIXlt(data$DateTime,format="%d/%m/%Y %H:%M:%S")
 data$Date=NULL
 data$Time=NULL
 
+subdata <- subset(data, DateTime$year==107 & DateTime$mon==1 & (DateTime$mday==1 | DateTime$mday==2))
+
 png("plot3.png", width = 480, height = 480)
 plot(x=(subdata$DateTime), y=subdata$Sub_metering_1, xlab=NA, ylab="Energy sub metering", type="l")
 lines(x=(subdata$DateTime), y=subdata$Sub_metering_2, col="red")
